ae0b62f77b3625385af56cabe9c6e5a26d3afcaa
Mario Garcia Valdez <mariosky@gmail.com>
2019-02-20 10:37:54 -0800

    Cambios

---
+++ tarea11/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace tarea11
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wsd!");
+        }
+    }
+}
